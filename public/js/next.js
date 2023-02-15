5dfd6bc576d7f16ec4fd3abd86629d214f945e77
kimB <k1014ttl@naver.com>
2023-02-15 22:32:13 +0900

    230215 22:32 db!

---
+++ public/js/next.js
@@ -0,0 +1,22 @@
+import BLOCKS from "./block.js";
+
+const nextblock = document.querySelector(".next_block > ul");
+
+const NEXT_GAME_ROWS = 5;
+const NEXT_GAME_COLS = 5;
+
+export function nextinit(){
+    for(let i = 0; i < NEXT_GAME_ROWS; i++){
+        prependNextNewLine()
+    }
+}
+export function prependNextNewLine() {
+    const li2 = document.createElement("li"); //20개의 행
+    const ul2 = document.createElement("ul"); //li를 포함한 행
+    for(let j = 0; j < NEXT_GAME_COLS; j++){
+        const matrix2 = document.createElement("li"); //셀을 10개 만들어줌
+        ul2.prepend(matrix2); //만든 셀(10개)을 ul에 넣기
+    }
+    li2.prepend(ul2) //li에 ul 넣기
+    nextblock.prepend(li2) //테트리스 판에 li 넣기
+}
